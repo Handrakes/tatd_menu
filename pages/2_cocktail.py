b4f2c4c02b1cf772123f5ff9f450cbebd37b7437
Handrake <50285355+Handrakes@users.noreply.github.com>
2023-07-20 00:50:46 +0900

    Update 2_cocktail.py

--- pages/2_cocktail.py
+++ pages/2_cocktail.py
@@ -21,6 +21,35 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.markdown('<p class = "word-spacing"><span>레몬드랍 * 6</span><span style = "margin : 10em;"></span><span>2.5</span></p>', unsafe_allow_html = True)
-st.markdown('<p class = "word-spacing"><span>가디언즈 오브 갤럭시</span><span style="margin: 9em;"></span><span>1.5</span></p>', unsafe_allow_html=True)
-st.markdown('<p class = "word-spacing"><span>데 킬러</span><span style="margin: 9em;"></span><span>1.6</span></p>', unsafe_allow_html=True)
+#st.markdown('<p class = "word-spacing"><span>레몬드랍 * 6</span><span style = "margin : 10em;"></span><span>2.5</span></p>', unsafe_allow_html = True)
+#st.markdown('<p class = "word-spacing"><span>가디언즈 오브 갤럭시</span><span style="margin: 9em;"></span><span>1.5</span></p>', unsafe_allow_html=True)
+#st.markdown('<p class = "word-spacing"><span>데 킬러</span><span style="margin: 9em;"></span><span>1.6</span></p>', unsafe_allow_html=True)
+
+st.markdown(
+    """
+    <style>
+    .left-corner {
+        text-align: left;
+        float: left;
+    }
+    .right-corner {
+        text-align: right;
+        float: right;
+    }
+    .clearfix::after {
+        content: "";
+        display: table;
+        clear: both;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+#st.markdown('<p class = "word-spacing"><div class="clearfix">', unsafe_allow_html=True)
+st.markdown('<div class="left-corner">레몬드랍</div><div class="right-corner">2.5</div>', unsafe_allow_html=True)
+#st.markdown('</div></p>', unsafe_allow_html=True)
+st.markdown('<div class="left-corner">가디언즈 오브 갤럭시</div><div class="right-corner">1.5</div>', unsafe_allow_html=True)
+#st.markdown('</div>', unsafe_allow_html=True)
+st.markdown('<div class="left-corner">데 킬러</div><div class="right-corner">1.6</div>', unsafe_allow_html=True)
+#st.markdown('</div>', unsafe_allow_html=True)
