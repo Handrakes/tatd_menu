1662539fb52b5786114892eb2d070b40226d89cc
Handrake <50285355+Handrakes@users.noreply.github.com>
2023-07-20 00:28:29 +0900

    Update 2_cocktail.py

--- pages/2_cocktail.py
+++ pages/2_cocktail.py
@@ -10,6 +10,17 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.markdown("레몬드랍 * 6")
-st.markdown('<p class = "markdown-spacing"><span>가디언즈 오브 갤럭시</span><span style="margin: 0 9em;"></span><span>1.5</span></p>', unsafe_allow_html=True)
-st.markdown('<p class = "markdown-spacing"><span>데 킬러</span><span style="margin: 0 100px;"></span><span>1.6</span></p>', unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .word-spacing {
+        line-height: 0.5em;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown('<p class = "word-spacing"><span>레몬드랍 * 6</span><span style = "margin : 10em;"></span><span>2.5</span></p>', unsafe_allow_html = True)
+st.markdown('<p class = "word-spacing"><span>가디언즈 오브 갤럭시</span><span style="margin: 9em;"></span><span>1.5</span></p>', unsafe_allow_html=True)
+st.markdown('<p class = "word-spacing"><span>데 킬러</span><span style="margin: 9em;"></span><span>1.6</span></p>', unsafe_allow_html=True)
