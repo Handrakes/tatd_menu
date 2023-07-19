5b6136fee0b945a0f490d9d5a3af683543a3fa94
Handrake <50285355+Handrakes@users.noreply.github.com>
2023-07-20 01:38:30 +0900

    Update 2_cocktail.py

--- pages/2_cocktail.py
+++ pages/2_cocktail.py
@@ -1,8 +1,6 @@
 import io
 import streamlit as st
 
-st.title("SIGNATURE")
-
 st.markdown(
     """
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,31 +11,23 @@ st.markdown(
 st.markdown(
     """
     <style>
-    .word-spacing {
-        line-height: 0.5em;
-        font-size: 5em;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-#st.markdown('<p class = "word-spacing"><span>레몬드랍*6</span><span style = "margin : 10em;"></span><span>2.5</span></p>', unsafe_allow_html = True)
-#st.markdown('<p class = "word-spacing"><span>가디언즈 오브 갤럭시</span><span style="margin: 9em;"></span><span>1.5</span></p>', unsafe_allow_html=True)
-#st.markdown('<p class = "word-spacing"><span>데 킬러</span><span style="margin: 9em;"></span><span>1.6</span></p>', unsafe_allow_html=True)
-
-st.markdown(
-    """
-    <style>
     .left-corner {
         text-align: left;
         float: left;
-        font-size : 1.2em;
+        font-size : 1.3em;
+        margin-top: 1em;
     }
     .right-corner {
         text-align: right;
         float: right;
-        font-size : 1.2em;
+        font-size : 1.3em;
+        margin-top : 1em;
+    }
+    .space{
+        font-size : 0.8em;
+        margin-top : 0em;
+        margin-bottom : 0.5em;
+        line-height : 0em;
     }
     .clearfix::after {
         content: "";
@@ -49,12 +39,16 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-#st.markdown('<p class = "word-spacing"><div class="clearfix">', unsafe_allow_html=True)
+st.title('SIGNATURE')
 st.markdown('<div class="left-corner">레몬드랍 * 6</div><div class="right-corner">2.5</div>', unsafe_allow_html=True)
-st.markdown('<span style = "font-size:0.7em;">달달하고 상큼한 원샷 칵테일</span>', unsafe_allow_html=True)
+st.markdown('<div class="space">달달하고 상큼한 원샷 칵테일</div>', unsafe_allow_html=True)
 st.image('images/manhattan.jpg')
 #st.markdown('</div></p>', unsafe_allow_html=True)
 st.markdown('<div class="left-corner">가디언즈 오브 갤럭시</div><div class="right-corner">1.5</div>', unsafe_allow_html=True)
+st.markdown('<div class="space">피나콜라다 베이스의 밀키스 맛 칵테일 (2.4%)</div>', unsafe_allow_html=True)
+st.image('images/guardians.jpg')
 #st.markdown('</div>', unsafe_allow_html=True)
 st.markdown('<div class="left-corner">데 킬러</div><div class="right-corner">1.6</div>', unsafe_allow_html=True)
+st.markdown('<div class="space">데킬라 베이스의 생라임과 허브가 들어간 칵테일</div>', unsafe_allow_html=True)
+st.image('images/jungle-bird.jpg')
 #st.markdown('</div>', unsafe_allow_html=True)
