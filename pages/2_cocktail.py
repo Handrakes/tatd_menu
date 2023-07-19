cc2715e38f15307111021c6361c4a3a5b8c514f8
Handrake <50285355+Handrakes@users.noreply.github.com>
2023-07-20 00:15:38 +0900

    Update 2_cocktail.py

--- pages/2_cocktail.py
+++ pages/2_cocktail.py
@@ -5,15 +5,11 @@ st.title("SIGNATURE")
 
 st.markdown(
     """
-    <style>
-    .markdown-spacing {
-        margin-bottom: 0;
-    }
-    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     """,
     unsafe_allow_html=True
 )
 
-st.markdown('<p class = "markdown-spacing"><span>레몬드랍*6</span><span style="margin: 0 10em;"></span><span>2.5</span></p>', unsafe_allow_html=True)
+st.markdown("레몬드랍 * 6")
 st.markdown('<p class = "markdown-spacing"><span>가디언즈 오브 갤럭시</span><span style="margin: 0 9em;"></span><span>1.5</span></p>', unsafe_allow_html=True)
 st.markdown('<p class = "markdown-spacing"><span>데 킬러</span><span style="margin: 0 100px;"></span><span>1.6</span></p>', unsafe_allow_html=True)
